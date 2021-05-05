ff38353b29e2f491122bd514007ed9ef6eae82b9
bitsquant <38315748+bitsquant@users.noreply.github.com>
2021-05-05 11:06:26 +0530

    Update getDataFromApi.js

--- src/actions/getDataFromApi.js
+++ src/actions/getDataFromApi.js
@@ -10,7 +10,7 @@ export const saveApiDataAction = (x) => dispatch => {
 export const getDataFromApi = (APIkey) => {
   if(APIkey){
   return function(dispatch, getState) {
-  	let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&from=2021-04-02&sortBy=publishedAt&pageSize=100&page=1&apiKey=`+APIkey
+  	let url = `https://newsapi.org/v2/everything?domains=techcrunch.com,thenextweb.com&from=2021-04-15&sortBy=publishedAt&pageSize=100&page=1&apiKey=`+APIkey
     return fetch(url)
       .then(data => data.json())
       .then(data => {
